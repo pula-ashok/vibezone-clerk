c4a1cf4301d748de21a80f4aae1d932a50cfcfe2
pula-ashok <ashokpula0307@gmail.com>
2025-01-03 11:22:59 +0530

    rightsidebar completed

--- src/components/layout/MainContainer.jsx
+++ src/components/layout/MainContainer.jsx
@@ -10,11 +10,11 @@ const MainContainer = ({ children }) => {
   const firstPath=currentPath.match(regex)?currentPath.match(regex)[0]:currentPath
   const title=pageTitles.find(page=>page.url===firstPath)?.title||''
   return (
-    <section className='flex flex-col flex-1 max-w-3xl px-4 py-10 md:px-10 lg:px-4 xl:px-20'>
+    <section className='flex flex-col flex-1 max-w-3xl px-4  md:px-10 lg:px-4 xl:px-20'>
       <Topbar />
       <div className='mt-6 mb-20'>
         <h1 className='mb-5 text-heading2-bold max-sm:text-heading3-bold text-light-1'>{title}</h1>
-        <div className='h-screen overflow-y-scroll custom-scrollbar bg-blue-600'>
+        <div className='h-screen overflow-y-scroll custom-scrollbar'>
         {children}
         </div>
       </div>
