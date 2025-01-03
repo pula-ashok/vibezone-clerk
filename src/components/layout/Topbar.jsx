3486378318acc133d65ad3593b1b2733c3826622
pula-ashok <ashokpula0307@gmail.com>
2025-01-03 10:25:32 +0530

    mobile view for topbar

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,5 +1,8 @@
 'use client'
-import { Add, Search } from '@mui/icons-material'
+import { SignedIn, SignOutButton } from '@clerk/nextjs'
+import { Add, Logout, Search } from '@mui/icons-material'
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
@@ -13,6 +16,18 @@ const Topbar = () => {
         <Search className='search-icon' onClick={() => console.log(search)}/>
       </div>
       <button className='create-post-btn' onClick={()=>router.push('/create-post')}><Add/><p>Create A Post</p></button>
+      <div className='flex gap-3'>
+        <SignedIn>
+          <SignOutButton>
+            <div className='flex cursor-pointer items-center md:hidden'>
+              <Logout sx={{ color: 'white', fontSize: '32px' }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+        <Link href={"/"} >
+        <Image src={"/assets/phucmai.png"} alt='profile' width={50} height={50} className='rounded-full md:hidden'/>
+        </Link>
+      </div>
     </div>
   )
 }
