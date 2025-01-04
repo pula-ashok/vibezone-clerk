28fbeb8c3201e2effca203bfecdea20499101c19
pula-ashok <ashokpula0307@gmail.com>
2025-01-04 15:13:06 +0530

    created the loader component

--- src/components/layout/LeftSidebar.jsx
+++ src/components/layout/LeftSidebar.jsx
@@ -6,10 +6,12 @@ import Menu from './Menu'
 import { SignedIn, SignOutButton, UserButton, useUser } from '@clerk/nextjs'
 import { Logout } from '@mui/icons-material'
 import { dark } from '@clerk/themes'
+import Loader from './Loader'
 
 const LeftSidebar = () => {
   const {isLoaded,user}=useUser()
-  return (
+  const [loading, setLoading] = useState(false)
+  return loading?<Loader/> :(
     <div className='flex flex-col gap-6 overflow-auto h-screen  left-0 top-0 sticky px-10 py-6 max-md:hidden custom-scrollbar'>
       <Link href={"/"}><Image src={'/assets/logo.png'} alt='logo' width={200} height={200}/></Link>
       <div className='flex flex-col gap-2'>
