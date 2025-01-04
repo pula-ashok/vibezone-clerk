0e0d8f78a64e956f03b31eb01f01326f100980db
pula-ashok <ashokpula0307@gmail.com>
2025-01-04 11:01:30 +0530

    completed user model and actions

---
+++ src/lib/actions/user.js
@@ -0,0 +1,37 @@
+
+import User from '@lib/models/User';
+import { connectDB } from '@lib/mongodb/mongoose';
+export const createOrUpdateUser=async(id,first_name,last_name,image_url,email_addresses,username)=>{
+    try {
+        await connectDB();
+        const user = await User.findOneAndUpdate(
+          { clerkId: id },
+          {
+            $set: {
+              clerkId: id,
+              firstName: first_name,
+              lastName: last_name,
+              profilePhoto: image_url,
+              email: email_addresses[0].email_address,
+              username: username,
+            },
+          },
+          {
+            upsert: true,
+            new: true,
+          }
+        );
+        await user.save();
+        return user;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const deleteUser=async(id)=>{
+    try {
+        await User.findOneAndDelete({clerkId:id})
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected=false;
+
+export const connectDB=async()=>{
+    mongoose.set('strictQuery', true);
+    if(isConnected){
+        console.log('MongoDB is already connected');
+        return;
+    }
+    try{
+        await mongoose.connect(process.env.MONGODB_URI,{dbName:'vibezone'});
+        isConnected=true;
+        console.log('MongoDB connected');
+    }catch(error){
+        console.log(error);
+    }   
+}
